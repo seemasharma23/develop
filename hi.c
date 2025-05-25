6eb77d02cfc4a85808e860bff3dbe9408e4221a7
Sagar2006Krishna <sagar.k1@s.amity.edu>
2025-05-25 15:18:14 +0530

    commiting t2

--- hi.c
+++ hi.c
@@ -1,9 +1,11 @@
+//contriibuted by sk 
 #include <stdio.h>
 int main()
 {
-    int a = 5, b = 3;
+    int a = 6, b = 4;
     int sum = a + b;
     printf("Hello, World!\n");
     printf("Sum: %d\n", sum);
+    
     return 0;
 }
